3b558f786f4178864a7f205ef3b599a75aad064b
keshavraminedi <kraminedi@albany.edu>
2020-09-06 15:35:45 -0400

    Updates

---
+++ precache-manifest.d3c0d54fbb99351d3192dcfb0094c311.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18a35f91e69b51af4b3e4c0a15ec930b",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "f9eb7935c2d237572a35",
+    "url": "/monsters/static/css/main.cb223cf8.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/monsters/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9eb7935c2d237572a35",
+    "url": "/monsters/static/js/main.94a7f877.chunk.js"
+  },
+  {
+    "revision": "3dab8aa598718cb8e8b3",
+    "url": "/monsters/static/js/runtime-main.f48c3991.js"
+  }
+]);
